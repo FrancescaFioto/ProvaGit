a46a6ad4719de1291cc30aed09552f31f6eff2c6
Antonio Fioto <francescafioto1712@gmail.com>
2024-10-04 11:06:05 +0200

    Primo commit

---
+++ StampaOperazioni/src/it/betalent/main/Main.java
@@ -0,0 +1,20 @@
+package it.betalent.main;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		int a = 16;
+		int b = 3;
+		
+		int somma = a + b;
+		int modulo = a % b;
+		
+		System.out.println("Numero 1: " + a);
+        System.out.println("Numero 2: " + b);
+        System.out.println("Somma: " + somma);
+        System.out.println("Modulo: " + modulo);
+
+	}
+
+}
